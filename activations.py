21035f013a720d352201a958b0afb02b097eacc8
RKMSU <ikarus5991@gmail.com>
2017-12-02 13:20:22 +0300

    Initial commit

---
+++ activations.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Implementation of activation functions used within neural networks
+
+class BaseActivationFunction(object):
+
+    def val(self, inputs):
+        """
+        Calculates values of activation function for given inputs
+        :param inputs: numpy array (vector or matrix)
+        :return: result, numpy array of inputs size
+        """
+        raise NotImplementedError('This function must be implemented within child class!')
+
+    def deriv(self, inputs):
+        """
+        Calculates first derivatives of activation function for given inputs
+        :param inputs: numpy array (vector or matrix)
+        :return: result, numpy array of inputs size
+        """
+        raise NotImplementedError('This function must be implemented within child class!')
+
+    def second_deriv(self, inputs):
+        """
+        Calculates second derivatives of activation function for given inputs
+        :param inputs: numpy array (vector or matrix)
+        :return: result, numpy array of inputs size
+        """
+        raise NotImplementedError('This function must be implemented within child class!')
+
+
+class LinearActivationFunction(BaseActivationFunction):
+    pass
+
+
+class SigmoidActivationFunction(BaseActivationFunction):
+    pass
+
+
+class ReluActivationFunction(BaseActivationFunction):
+    pass
+
