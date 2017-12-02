7c3dc5b327371e0e16a5a2ea1aade08b3eb254a1
RKMSU <ikarus5991@gmail.com>
2017-12-02 13:42:04 +0300

    acts added

--- activations.py
+++ activations.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # Implementation of activation functions used within neural networks
+from scipy.special import expit
+
 
 class BaseActivationFunction(object):
 
@@ -31,13 +33,36 @@ class BaseActivationFunction(object):
 
 
 class LinearActivationFunction(BaseActivationFunction):
-    pass
+    def val(self, inputs):
+        return inputs
+
+    def deriv(self, inputs):
+        return np.ones_like(inputs)
+
+    def second_deriv(delf, inputs):
+        return np.zeros_like(inputs)
 
 
 class SigmoidActivationFunction(BaseActivationFunction):
-    pass
+    def val(self, inputs):
+        return expit(inputs)
+
+    def deriv(self, inputs):
+        _val = self.val(inouts)
+        return _val * (1 - _val)
+
+    def second_deriv(self, inputs):
+        _val = self.deriv(inputs)
+        _deriv = self.val(inputs)
+        return _deriv(1 - 2 * _val)
 
 
 class ReluActivationFunction(BaseActivationFunction):
-    pass
+    def val(self, inputs):
+        return np.vectorize(lambda t: t if t > 0 else 0)
 
+    def deriv(self, inputs):
+        return np.vectorize(lambda t: 1 if t > 0 else 0)
+
+    def second_deriv(self, inputs):
+        return np.zeros_like(inputs)
