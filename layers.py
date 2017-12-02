6f89f9e0b56acf8fb325a65289dc81a4d6913d2a
RKMSU <ikarus5991@gmail.com>
2017-12-02 14:10:27 +0300

    layers added

--- layers.py
+++ layers.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # Implementation of layers used within neural networks
+import numpy as np
+from numpy.random import normal
 
 class BaseLayer(object):
 
@@ -82,5 +84,63 @@ class FCLayer(BaseLayer):
         :param afun: layer activation function, instance of BaseActivationFunction
         :param use_bias: flag for using bias parameters
         """
-        pass
+        self.shape = shape
+        self.afun = afun
+        self.use_bias = use_bias
 
+        scale = (1. / (shape[0] + shape[1]))
+        size = shape if not use_bias else (shape[1], shape[0] + 1)
+        self.W = normal(loc=0, scale=scale, size=size)
+
+    def get_params_number(self):
+        if self.use_bias:
+            return (self.shape[0] + 1) * self.shape[1]
+        else:
+            return self.shape[0] * self.shape[1]
+
+    def get_weights(self):
+        return np.ravel(self.W)
+
+    def set_weights(self, w):
+        self.W = w.reshape(self.shape[1], self.shape[0] + self.use_bias)
+
+    def set_direction(self, p):
+        self.p = p.reshape(self.shape[1], self.shape[0] + self.use_bias)
+
+    def get_activations(self):
+        return self.zL
+
+    def forward(self, inputs):
+        self.z = inputs
+        if self.use_bias:
+            self.z = np.vstack((inputs, np.ones(inputs.shape[1])))
+
+        self.inputs = self.z
+        self.uL = self.W.dot(self.z)
+        self.zL = self.afun.val(self.uL)
+        return self.zL
+
+    def backward(self, derivs):
+        self.gz = derivs
+        self.deriv_uL = self.afun.deriv(self.uL)
+        self.gul = self.gz * self.deriv_uL
+        gwl = self.gul.dot(self.z.T)
+        gzl = self.W[:, :self.W.shape[1] - self.use_bias].T.dot(self.gul)
+        return gzl, np.ravel(gwl)
+
+    def Rp_forward(self, Rp_inputs):
+        self.Rpz = Rp_inputs
+        if self.use_bias:
+            self.Rpz = np.vstack((Rp_inputs, np.zeros(Rp_inputs.shape[1])))
+
+        self.Rpul = self.W.dot(self.Rpz) + self.p.dot(self.z)
+        Rpzl = self.deriv_uL * self.Rpul
+        return Rpzl
+
+    def Rp_backward(self, Rp_derivs):
+        Rpdul = Rp_derivs * self.deriv_uL + \
+            self.gz * self.afun.second_deriv(self.uL) * self.Rpul
+        Rpdwl = Rpdul.dot(self.z.T) + self.gul.dot(self.Rpz.T)
+        Rpdzl = self.W[:, :self.W.shape[1] - self.use_bias].T.dot(Rpdul) +\
+            self.p[:, :self.p.shape[1] - self.use_bias].T.dot(self.gul)
+        return Rpdzl, np.ravel(Rpdwl)
